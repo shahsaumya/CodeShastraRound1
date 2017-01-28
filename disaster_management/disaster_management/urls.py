f845a375cb4e65677392689627781b48c45c7ad5
Saumya Shah <shahsaumya0108@gmail.com>
2017-01-29 03:29:42 +0530

    Static CDN

--- disaster_management/disaster_management/urls.py
+++ disaster_management/disaster_management/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from disaster import views
+from django.conf.urls.static import static
 from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     # Examples:
@@ -11,3 +12,7 @@ urlpatterns = [
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOTT)
+	
